eb6b9486e0887a0687791554f2d4abecc52081c4
krisliiv <krisliiv@greeny.cs.tlu.ee>
2016-09-22 11:03:12 +0300

    Added email memory

---
+++ login2.php
@@ -0,0 +1,72 @@
+<?php 
+	//var_dump($_GET);
+	//echo "<br>";
+	//var_dump($_POST);
+	
+	$signupEmailError = "";
+	$signupEmail = "";
+	
+	//kas on üldse olemas
+	if (isset ($_POST["signupEmail"])) {
+		
+		// oli olemas, ehk keegi vajutas nuppu
+		// kas oli tühi
+		if (empty ($_POST["signupEmail"])) {
+			
+			//oli tõesti tühi
+			$signupEmailError = "See väli on kohustuslik";
+			
+		} else {
+			
+			//oli olemas, kõik korras
+			$signupEmail = $_POST["signupEmail"];	
+		}
+		
+		
+	}
+	
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sisselogimise leht</title>
+	</head>
+	<body>
+
+		<h1>Logi sisse</h1>
+		
+		<form method="POST">
+			
+			<label>E-post</label><br>
+			<input name="loginEmail" type="email">
+			
+			<br><br>
+			
+			<label>Parool</label><br>
+			<input name="loginPassword" type="password">
+						
+			<br><br>
+			
+			<input type="submit">
+		
+		</form>
+		
+		<h1>Loo kasutaja</h1>
+		
+		<form method="POST">
+			
+			<label>E-post</label><br>
+			<input name="signupEmail" type="email" value="<?php echo $signupEmail?>"> <?php echo $signupEmailError; ?>
+			
+			<br><br>
+			
+			<input placeholder="Parool" name="signupPassword" type="password">
+						
+			<br><br>
+			
+			<input type="submit" value="Loo kasutaja">
+		
+		</form>
+
+	</body>
+</html>
